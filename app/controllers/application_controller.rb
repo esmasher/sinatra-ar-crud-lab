a374cb62df2f09936cdc307793d7511053c03f76
esmasher <esmasherr@gmail.com>
2021-01-13 16:10:24 -0600

    commit tree ff0f224de2cd89e7111fb1e7d96a26ef94b70914

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,45 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+#read
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+#read
+  get '/article/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+#create
+  get 'articles/new' do
+    @article = Article.new
+    erb :new
+  end
+#create
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to '/articles/#{@article.id}'
+  end
+#update
+  get 'articles/:id/edit' do
+    @article = Article.find(param[:id])
+    erb :edit
+  end
+#update
+  patch 'articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:id])
+    redirect to '/articles/#{article.id}'
+  end
+#delete
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to '/articles'
+  end
+
+
+
 end
